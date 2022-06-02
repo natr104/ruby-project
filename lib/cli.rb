e1074a2560a80b4f37f708f6c8d9e5e1f1727bac
Nathan <troy.nathan@gmail.com>
2022-06-02 21:56:36 +1000

    started adding to CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,8 +4,28 @@ class CLI
     end
 
     def start
+        puts ""
         puts "Welcome to the Australian Federal Election 2022 Results Tracker"
+        puts ""
+
+    end
+
+    def print_electorate electorate
+        system("clear")
+        puts "=================================="
+        puts "Division of #{electorate.division} in #{electorate.state}."
+        puts "=================================="
+        puts "Previously held by: #{electorate.incumbent}. Leading party: #{electorate.leading} by a margin of #{electorate.margin} votes. Two-party-preferred: #{electorate.tcp}% with a swing of #{electorate.swing}%."
         puts ""
     end
 
+    def list_electorates
+
+    end
+
+    def list_electorates_by_state
+
+    end
+
+    
 end
